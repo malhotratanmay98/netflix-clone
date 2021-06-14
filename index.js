8fc0fe70032fc68b87d3fb6b81610d21fd5ea96f
malhotratanmay98 <61630303+malhotratanmay98@users.noreply.github.com>
2021-06-14 16:08:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Hello! I am an alert box!!");
+
+document.querySelector('#card1').addEventListener('click', () => {
+    var element = document.getElementById('card1');
+    element.classList.toggle("ontouch-card");
+  }); 
+
+  document.querySelector('#card2').addEventListener('click', () => {
+    var element = document.getElementById('card2');
+    element.classList.toggle("ontouch-card");
+  }); 
